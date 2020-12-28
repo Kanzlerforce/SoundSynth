9e5d1aa6377d162fa400844c2cf07ce5a66410b7
Chris <cansler7@gmail.com>
2020-12-28 13:27:36 -0500

    video 1 complete

--- SoundSynth/main.cpp
+++ SoundSynth/main.cpp
@@ -3,14 +3,22 @@ using namespace std;
 
 #include "olcNoiseMaker.h";
 
-double makeNoise(double dTime) {
-	double dOutput = 1.0 * sin(440.0 * 2 * 3.14159 * dTime);
+atomic<double> dFrequencyOutput = 0.0;
 
+double makeNoise(double dTime) {
+	double dOutput = sin(dFrequencyOutput * 2 * 3.14159 * dTime);
+	dOutput += sin((dFrequencyOutput + 20.0) * 2 * 3.14159 * dTime);
+	dOutput *= 1.0;
+
+	return dOutput * 0.4;
+
+	/*
 	if(dOutput > 0.0) {
 		return 0.2;
 	} else {
 		return -0.2;
 	}
+	*/
 }
 
 int main() {
@@ -29,8 +37,24 @@ int main() {
 	// Link noise function with sound machine
 	sound.SetUserFunction(makeNoise);
 
-	while(1) {
+	double dOctaveBaseFrequency = 110.0;	// A2
+	double d12thRoofOf2 = pow(2.0, 1.0 / 12.0); 
 
+	while(1) {
+		// Add a keyboard like a piano
+		// \xbcL is comma
+		// \xbe is period
+		bool bKeyPressed = false;
+		for (int k = 0; k < 15; k++) {
+			if(GetAsyncKeyState((unsigned char)("ZSXCFVGBNJMK\xbcL\xbe"[k])) & 0x8000) {
+				dFrequencyOutput = dOctaveBaseFrequency * pow(d12thRoofOf2, k);
+				bKeyPressed = true;
+			} 
+		}
+
+		if(!bKeyPressed) {
+			dFrequencyOutput = 0.0;
+		}
 	}
 
 	return 0;
